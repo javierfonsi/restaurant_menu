7f3609c2d0f1709c55d4bff30cb79b9e149151b5
javierfonsi <javier_fonsi@hotmail.com>
2022-09-04 20:06:25 -0500

    adminusers

--- routes/adminusers.routes.js
+++ routes/adminusers.routes.js
@@ -97,141 +97,141 @@ const router = express.Router()
  */
  router.post('/', postAdminUser);
 
- //Login adminUser
- /**
-  * @swagger
-  * /api/v1/adminuser/login:
-  *  post:
-  *    summary: allow auth an adminUser
-  *    tags: [adminuser]
-  *    requestBody: 
-  *      required: true
-  *      content:
-  *          application/json:
-  *              schema:
-  *                type: object
-  *                $ref: '#/components/schemas/loggedAdminUser'
-  *    responses:
-  *      201:
-  *        description: new adminUser was created!
-  *      400:
-  *        description: some properties and/or their values are incorrect
-  */
- router.post('/login', loginAdminUser);
+// //Login adminUser
+// /**
+//  * @swagger
+//  * /api/v1/adminuser/login:
+//  *  post:
+//  *    summary: allow auth an adminUser
+//  *    tags: [adminuser]
+//  *    requestBody: 
+//  *      required: true
+//  *      content:
+//  *          application/json:
+//  *              schema:
+//  *                type: object
+//  *                $ref: '#/components/schemas/loggedAdminUser'
+//  *    responses:
+//  *      201:
+//  *        description: new adminUser was created!
+//  *      400:
+//  *        description: some properties and/or their values are incorrect
+//  */
+// router.post('/login', loginAdminUser);
  
- router.use(validateSession);
+// router.use(validateSession);
  
- // get all adminUser
- /**
-  * @swagger
-  * /api/v1/adminuser:
-  *  get:
-  *    security:
-  *      - bearerAuth: []
-  *    summary: returns all adminUser which status are active
-  *    tags: [adminuser]
-  *    responses:
-  *      200:
-  *        description: All adminUser
-  *        content:
-  *          application/json:
-  *              schema:
-  *                  type: array
-  *                  items:
-  *                    $ref: '#/components/schemas/adminuser'
-  */
+// // get all adminUser
+// /**
+//  * @swagger
+//  * /api/v1/adminuser:
+//  *  get:
+//  *    security:
+//  *      - bearerAuth: []
+//  *    summary: returns all adminUser which status are active
+//  *    tags: [adminuser]
+//  *    responses:
+//  *      200:
+//  *        description: All adminUser
+//  *        content:
+//  *          application/json:
+//  *              schema:
+//  *                  type: array
+//  *                  items:
+//  *                    $ref: '#/components/schemas/adminuser'
+//  */
  
- router.get('/', getAllAdminUser);
+// router.get('/', getAllAdminUser);
  
- router.use('/:id', adminUserExist);
- //router.route('/:id')
+// router.use('/:id', adminUserExist);
+// //router.route('/:id')
  
- //Get adminUser by Id
- /**
-  * @swagger
-  * /api/v1/adminuser/{id}:
-  *  get:
-  *    security:
-  *      - bearerAuth: []
-  *    summary: returns an adminUser by id
-  *    tags: [adminuser]
-  *    parameters:
-  *      - in: path
-  *        name: id
-  *        schema:
-  *          type: string
-  *        required: true
-  *        description: the adminuser id
-  *    responses:
-  *      200:
-  *        description: Return adminUser with id
-  *        content:
-  *          application/json:
-  *              schema:
-  *                  type: object
-  *                  items:
-  *                    $ref: '#/components/schemas/adminuser'
-  *      404:
-  *        description: The delivered adminUser id was not found.
-  */
+// //Get adminUser by Id
+// /**
+//  * @swagger
+//  * /api/v1/adminuser/{id}:
+//  *  get:
+//  *    security:
+//  *      - bearerAuth: []
+//  *    summary: returns an adminUser by id
+//  *    tags: [adminuser]
+//  *    parameters:
+//  *      - in: path
+//  *        name: id
+//  *        schema:
+//  *          type: string
+//  *        required: true
+//  *        description: the adminuser id
+//  *    responses:
+//  *      200:
+//  *        description: Return adminUser with id
+//  *        content:
+//  *          application/json:
+//  *              schema:
+//  *                  type: object
+//  *                  items:
+//  *                    $ref: '#/components/schemas/adminuser'
+//  *      404:
+//  *        description: The delivered adminUser id was not found.
+//  */
  
-  router.get("/:id", getAdminUserById)
+//  router.get("/:id", getAdminUserById)
  
- // patch adminUser by Id
- /**
-  * @swagger
-  * /api/v1/adminuser/{id}:
-  *  patch:
-  *    security:
-  *      - bearerAuth: []
-  *    summary: Allows update some adminUser properties
-  *    tags: [adminuser]
-  *    parameters:
-  *      - in: path
-  *        name: id
-  *        schema:
-  *          type: string
-  *        required: true
-  *        description: the adminUser id
-  *    requestBody:
-  *      description: Update adminUser with selected properties
-  *      required: true
-  *      content:
-  *          application/json:
-  *              schema:
-  *                type: properties
-  *                $ref: '#/components/schemas/adminuser'
-  *    responses:
-  *      204:
-  *        description: The selected adminUser id was modified partially
-  *      404:
-  *        description: The delivered adminUser id was not found.
-  */
+// // patch adminUser by Id
+// /**
+//  * @swagger
+//  * /api/v1/adminuser/{id}:
+//  *  patch:
+//  *    security:
+//  *      - bearerAuth: []
+//  *    summary: Allows update some adminUser properties
+//  *    tags: [adminuser]
+//  *    parameters:
+//  *      - in: path
+//  *        name: id
+//  *        schema:
+//  *          type: string
+//  *        required: true
+//  *        description: the adminUser id
+//  *    requestBody:
+//  *      description: Update adminUser with selected properties
+//  *      required: true
+//  *      content:
+//  *          application/json:
+//  *              schema:
+//  *                type: properties
+//  *                $ref: '#/components/schemas/adminuser'
+//  *    responses:
+//  *      204:
+//  *        description: The selected adminUser id was modified partially
+//  *      404:
+//  *        description: The delivered adminUser id was not found.
+//  */
  
- router.patch("/:id", protectAccountOwner, patchAdminUserById)
+// router.patch("/:id", protectAccountOwner, patchAdminUserById)
  
- // delete adminUser by Id
- /**
-  * @swagger
-  * /api/v1/adminuser/{id}:
-  *  delete:
-  *    security:
-  *      - bearerAuth: []
-  *    summary: delete a adminUser using soft-delete
-  *    tags: [adminuser]
-  *    parameters:
-  *      - in: path
-  *        name: id
-  *        schema:
-  *          type: string
-  *        required: true
-  *        description: Type the adminUser id to delete
-  *    responses:
-  *      204:
-  *        description: The selected adminUser id was deleted.
-  *      404:
-  *        description: The delivered adminUser id was not found.
-  */
-   .delete("/:id", protectAccountOwner, deleteAdminUserById);
+// // delete adminUser by Id
+// /**
+//  * @swagger
+//  * /api/v1/adminuser/{id}:
+//  *  delete:
+//  *    security:
+//  *      - bearerAuth: []
+//  *    summary: delete a adminUser using soft-delete
+//  *    tags: [adminuser]
+//  *    parameters:
+//  *      - in: path
+//  *        name: id
+//  *        schema:
+//  *          type: string
+//  *        required: true
+//  *        description: Type the adminUser id to delete
+//  *    responses:
+//  *      204:
+//  *        description: The selected adminUser id was deleted.
+//  *      404:
+//  *        description: The delivered adminUser id was not found.
+//  */
+//   .delete("/:id", protectAccountOwner, deleteAdminUserById);
  
  module.exports = { adminUsersRouter: router };
